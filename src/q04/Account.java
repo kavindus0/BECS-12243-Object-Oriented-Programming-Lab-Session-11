3575f48869958260d48efade990c46290b2c08d7
kavindus0 <kavindusachinthe@outlook.com>
2025-01-06 14:27:34 +0530

    new methods Added

--- src/q04/Account.java
+++ src/q04/Account.java
@@ -5,4 +5,19 @@ public class Account {
     protected String accountHolderName;
     protected double balance;
 
+    public Account(String accountNumber, String accountHolderName, double balance) {
+        this.accountNumber = accountNumber;
+        this.accountHolderName = accountHolderName;
+        this.balance = balance;
+    }
+
+    public void deposit(double amount) {
+        this.balance +=amount;
+    }
+    public void displayAccountDetails() {
+        System.out.println(accountNumber);
+        System.out.println(accountHolderName);
+        System.out.println(balance);
+    }
+
 }
