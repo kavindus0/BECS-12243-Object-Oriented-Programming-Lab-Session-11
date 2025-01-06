5ed011f9bae5a864948e3f3712d9e6358b80fca0
kavindus0 <kavindusachinthe@outlook.com>
2025-01-06 21:10:31 +0530

    print detais added

--- src/q02/Member.java
+++ src/q02/Member.java
@@ -13,4 +13,12 @@ public class Member {
         return salary;
     }
 
+    
+    public void printDetails() {
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Phone Number: " + phoneNumber);
+        System.out.println("Address: " + address);
+        System.out.println("Salary: " + salary);
+    }
 }
