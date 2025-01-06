ce318502a50615930555548f05ffb934630a4d03
kavindus0 <kavindusachinthe@outlook.com>
2025-01-06 13:52:29 +0530

    override Substraction

---
+++ src/q05/Subtraction.java
@@ -0,0 +1,18 @@
+package q05;
+
+public class Subtraction {
+
+
+    public int Subtraction(int a, int b) {
+        return a - b;
+    }
+    public double Subtraction(double a, double b) {
+        return a - b;
+    }
+    public double Subtraction(int a, double b) {
+        return a - b;
+    }
+    public double Subtraction(double a, int b) {
+        return a - b;
+    }
+}
