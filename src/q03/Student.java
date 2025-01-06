9de93891cd233a787e9883ff3fb4c9f508f960a5
kavindus0 <kavindusachinthe@outlook.com>
2025-01-06 19:14:50 +0530

    refactored Codes

--- src/q03/Student.java
+++ src/q03/Student.java
@@ -6,11 +6,6 @@ public class Student extends User {
     public static void main(String[] args) {
         Student std = new Student();
         std.viewCourses();
-
-
-
-
-
     }
     void viewCourses(){
         String Courses[] = new String[] {"Course01","Course02"};
