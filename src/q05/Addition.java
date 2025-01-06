c47759da1f39ec163e20de5285e494c0150fea60
kavindus0 <kavindusachinthe@outlook.com>
2025-01-06 13:17:16 +0530

    Addition Class created for Calculator

---
+++ src/q05/Addition.java
@@ -0,0 +1,19 @@
+package q05;
+
+public class Addition {
+    private int a;
+    private int b;
+
+    public int Addition(int a, int b) {
+        return this.a + this.b;
+    }
+    public double Addition(double a, double b) {
+        return this.a + this.b;
+    }
+    public double Addition(int a, double b) {
+        return this.a + this.b;
+    }
+    public double Addition(double a, int b) {
+        return this.a + this.b;
+    }
+}
