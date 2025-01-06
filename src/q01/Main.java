bcfe76d2692c8dc3fb2e4b89112117af577942db
kavindus0 <kavindusachinthe@outlook.com>
2025-01-06 20:40:04 +0530

    New codes added

--- src/q01/Main.java
+++ src/q01/Main.java
@@ -3,9 +3,11 @@ package q01;
 public class Main {
     public static void main(String[] args) {
         Parent parentObject = new Parent();
-        parentObject.show();
         Child childObject = new Child();
+        parentObject.show();
+        childObject.display();
         childObject.show();
+      //  parentObject.display();
 
     }
 }
