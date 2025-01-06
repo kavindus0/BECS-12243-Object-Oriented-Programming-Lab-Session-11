b500c471c5b4e37558135ece512a886f508ece72
kavindus0 <kavindusachinthe@outlook.com>
2025-01-06 14:54:49 +0530

    new methods added

--- src/q04/CurrentAccount.java
+++ src/q04/CurrentAccount.java
@@ -1,4 +1,22 @@
 package q04;
 
-public class CurrentAccount {
+public class CurrentAccount extends Account{
+   double  creditLimit ;
+    public CurrentAccount(String accountNumber, String accountHolderName, double balance) {
+        super(accountNumber, accountHolderName, balance);
+    }
+
+    public void setCreditLimit(double creditLimit) {
+        this.creditLimit = creditLimit;
+    }
+
+    public void withdraw(double amount){
+        if (amount >= super.balance && amount<=creditLimit){
+            System.out.println("Withrawn Successfully "+amount);
+            super.balance-=amount;
+        } else {
+            System.out.println("Unavailable RN");
+        }
+        
+    }
 }
