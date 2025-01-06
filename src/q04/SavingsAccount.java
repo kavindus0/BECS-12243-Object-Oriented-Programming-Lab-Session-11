1780a1475ef365cdfa3fdbd715c4b4f4d38484f4
kavindus0 <kavindusachinthe@outlook.com>
2025-01-06 14:54:43 +0530

    new methods added

--- src/q04/SavingsAccount.java
+++ src/q04/SavingsAccount.java
@@ -1,4 +1,28 @@
 package q04;
 
-public class SavingsAccount {
+public class SavingsAccount extends Account {
+
+    double interestRate;
+
+    public SavingsAccount(String accountNumber, String accountHolderName, double balance) {
+        super(accountNumber, accountHolderName, balance);
+    }
+
+    public void withdraw(double amount){
+        if (super.balance >=0){
+            System.out.println("Withdrawn Successfully "+amount);
+            super.balance -= amount;
+        } else {
+            System.out.println("Insufficient Balance");
+        }
+    }
+
+
+    public void applyInterest(){
+        double Interest = super.balance * interestRate / 100;
+        System.out.println("Interest is "+Interest+ " And Applying For Your Account");
+        super.balance += Interest;
+    }
+
+
 }
