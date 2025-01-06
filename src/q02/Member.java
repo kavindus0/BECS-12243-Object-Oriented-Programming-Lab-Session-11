0da959b8b5d1f2c5c8b1d92fa034c72a9f42fe29
kavindus0 <kavindusachinthe@outlook.com>
2025-01-06 20:54:48 +0530

    Member class created

---
+++ src/q02/Member.java
@@ -0,0 +1,14 @@
+package q02;
+
+public class Member {
+    private String name;
+    private int age;
+    private String phoneNumber;
+    private String address;
+    private double salary;
+
+    public double printSalary() {
+        return salary;
+    }
+
+}
