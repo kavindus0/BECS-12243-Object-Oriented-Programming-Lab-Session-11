64ff9096e78aadb5c93afa5defc70db85e115279
kavindus0 <kavindusachinthe@outlook.com>
2025-01-06 21:09:43 +0530

    Constructor added

--- src/q02/Manager.java
+++ src/q02/Manager.java
@@ -1,7 +1,13 @@
 package q02;
 
 public class Manager extends Member {
-}
+    private String department;
+
+    public Manager(String name, int age, String phoneNumber, String address,
+                  double salary, String department) {
+        super(name, age, phoneNumber, address, salary);
+        this.department = department;
+    }
 
     public void printDetails() {
         super.printDetails();
