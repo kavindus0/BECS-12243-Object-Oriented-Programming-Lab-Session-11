9e8c5ad5ac9eb16e6e1ea43312b03c225e356f45
kavindus0 <kavindusachinthe@outlook.com>
2025-01-06 13:56:10 +0530

    new code addded

--- src/q05/Calculator.java
+++ src/q05/Calculator.java
@@ -5,10 +5,9 @@ public class Calculator {
         Addition add = new Addition();
         Subtraction sub = new Subtraction();
         Division div = new Division();
-        System.out.println(add.Addition(5, 7));
-        System.out.println(add.Addition(5.0, 7));
+
         System.out.println(add.Addition(5, 7.0));
-        System.out.println(add.Addition(5.0, 7.0));
+
         System.out.println(sub.Subtraction(95,15.5));
         System.out.println(div.Division(5,0));
         System.out.println(div.Division(15,0.5));
