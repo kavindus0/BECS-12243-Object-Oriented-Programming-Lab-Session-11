739c60f63753e7ffd42c79ad6e3a623b50945c8d
kavindus0 <kavindusachinthe@outlook.com>
2025-01-06 13:52:02 +0530

    override add methods

--- src/q05/Addition.java
+++ src/q05/Addition.java
@@ -1,19 +1,17 @@
 package q05;
 
 public class Addition {
-    private int a;
-    private int b;
 
     public int Addition(int a, int b) {
-        return this.a + this.b;
+        return a + b;
     }
     public double Addition(double a, double b) {
-        return this.a + this.b;
+        return a + b;
     }
     public double Addition(int a, double b) {
-        return this.a + this.b;
+        return a + b;
     }
     public double Addition(double a, int b) {
-        return this.a + this.b;
+        return a + b;
     }
 }
