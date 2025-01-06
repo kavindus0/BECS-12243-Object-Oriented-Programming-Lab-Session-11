025a3929bb41a8a89c984e498c3b31ac4af27684
kavindus0 <kavindusachinthe@outlook.com>
2025-01-06 21:09:32 +0530

    print details method created

--- src/q02/Manager.java
+++ src/q02/Manager.java
@@ -2,3 +2,9 @@ package q02;
 
 public class Manager extends Member {
 }
+
+    public void printDetails() {
+        super.printDetails();
+        System.out.println("Department: " + department);
+    }
+}
