f1cacb1eb5bce9fd1e5de147aaa91b69f9b2ac0c
kavindus0 <kavindusachinthe@outlook.com>
2025-01-06 19:14:24 +0530

    2 References Created

---
+++ src/q01/Main.java
@@ -0,0 +1,11 @@
+package q01;
+
+public class Main {
+    public static void main(String[] args) {
+        Parent parentObject = new Parent();
+        parentObject.show();
+        Child childObject = new Child();
+        childObject.show();
+
+    }
+}
