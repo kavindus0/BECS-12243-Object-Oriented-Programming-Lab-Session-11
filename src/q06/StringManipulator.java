5a3b13c22f748ec5be5a2616998df343c20ac5fc
kavindus0 <kavindusachinthe@outlook.com>
2025-01-06 13:58:10 +0530

    new class created

---
+++ src/q06/StringManipulator.java
@@ -0,0 +1,19 @@
+package q06;
+
+public class StringManipulator {
+    public String reverse(String s){
+
+        return s;
+    }
+    // – Reverses a string.
+           public String reverse(String s, int n){
+
+               return s;
+           }
+    // – Reverses the first n characters of a string.
+    public String reverse(String s, int start, int end){
+
+        return s;
+    }
+    //– Reverses a substring
+}
