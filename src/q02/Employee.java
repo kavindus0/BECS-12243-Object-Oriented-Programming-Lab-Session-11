2dd7ba12cfb47f872025ec41b2869af3f8380a7f
kavindus0 <kavindusachinthe@outlook.com>
2025-01-06 21:11:14 +0530

    constructor and method added

--- src/q02/Employee.java
+++ src/q02/Employee.java
@@ -1,4 +1,16 @@
 package q02;
 
 public class Employee extends Member {
+    private String specialization;
+
+    public Employee(String name, int age, String phoneNumber, String address,
+                   double salary, String specialization) {
+        super(name, age, phoneNumber, address, salary);
+        this.specialization = specialization;
+    }
+
+    public void printDetails() {
+        super.printDetails();
+        System.out.println("Specialization: " + specialization);
+    }
 }
